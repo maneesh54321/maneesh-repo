2735e9ea7da63b7bbb14043928f20b7222825bb4
maneesh54321 <maneesh54321@gmail.com>
2017-05-18 17:09:58 +0530

    working Spring App

---
+++ MyFirstSpringApp/WebContent/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>My WebSite</title>
+<script src="js/angular.min.js"></script>
+<script src="js/home.js"></script>
+</head>
+<body data-ng-controller="homePageController as Ctrl">
+	<header>
+		<h3>My Website's Header</h3>
+	</header>
+	<section>
+		<p>This is the body</p>
+	</section>
+	<footer>
+		<h6>My Website's Footer</h6>
+	</footer>
+</html>
--- MyFirstSpringApp/src/main/java/com/spring/controller/SpringAppController.java
+++ MyFirstSpringApp/src/main/java/com/spring/controller/SpringAppController.java
@@ -1,5 +1,8 @@
 package com.spring.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,15 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.spring.service.SpringAppService;
 
 @Controller
-//@RequestMapping("/controller")
+@RequestMapping("/controller")
 public class SpringAppController {
 
 	@Qualifier("springAppService")
 	private SpringAppService springAppService;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String root() {
-		System.out.println("aaya yeha");
+	@RequestMapping(value = "/getData", method = RequestMethod.GET)
+	public String getData() {
+		Logger.getLogger("MyFirstSpringApp").log(Level.INFO, "Entered the Controller");
 		return "home";
 	}
 }
