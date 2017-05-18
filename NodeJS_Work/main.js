7bd73faca2e82de50b5cceca2c7f4bfc654b5b44
Maneesh Singh01 <Maneesh_Singh01@ad.infosys.com>
2017-05-18 09:33:31 +0530

    node learning experiments on buffers

--- NodeJS_Work/main.js
+++ NodeJS_Work/main.js
@@ -23,32 +23,69 @@ fs.readFile('input.txt', function (err, data) {
 console.log("Program Ended");*/
 
 
-
-// Import events module
 var events = require('events');
 
-
-// Create an eventEmitter object
 var eventEmitter = new events.EventEmitter();
 
-// Create an event handler as follows
-var connectHandler = function connected() {
-   console.log('connection succesful.');
-  
-   // Fire the data_received event 
-   eventEmitter.emit('data_received');
+var connectHandler1 = function connected(data1) {
+	console.log(data1);
+	console.log('connection succesful1.');
+	eventEmitter.emit('data_received');
 }
 
+var connectHandler2 = function connected(data1,data2) {
+	console.log(data2);
+	console.log('connection succesful2.');
+	eventEmitter.emit('data_received');
+}
+
+var handler=function handle(){
+	console.log("New Listener added");
+};
+eventEmitter.on('newListener',handler);
+
 // Bind the connection event with the handler
-eventEmitter.on('connection', connectHandler);
+eventEmitter.on('connection', connectHandler1);
  
 // Bind the data_received event with the anonymous function
 eventEmitter.on('data_received', function(){
    console.log('data received succesfully.');
 });
 
+var handler1=function handle(){
+	console.log("Listener removed");
+};
+eventEmitter.on('removeListener',handler1);
 // Fire the connection event 
-eventEmitter.emit('connection');
+//eventEmitter.emit('connection');
+eventEmitter.removeListener('connection',connectHandler1);
+//eventEmitter.emit('connection');
+eventEmitter.on('connection', connectHandler2);
 
+//eventEmitter.emit('connection');
+var data="abcd";
+eventEmitter.emit('connection',"abcd","efgh");
+
+console.log(eventEmitter.listeners('connection'));
+console.log(events.listenerCount(eventEmitter,'connection'));
 console.log("Program Ended.");
->>>>>>> Stashed changes
+
+
+
+
+var EE = require('events').EventEmitter;
+var ee = new EE();
+
+ee.on('test', function (first, second, third) {
+  console.log(first, second, third); //Will output full phrase
+});
+
+ee.on('test', function (first) {
+  console.log(first); //Will output just first word
+});
+
+ee.on('test', function () {
+  console.log.apply(console, arguments); //Will output full phrase again
+});
+
+ee.emit('test', 'Hello', 'my', 'world!');
