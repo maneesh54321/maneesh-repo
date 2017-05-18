2aa8d65b7d808851552cc33a851f51b300af17ba
Maneesh Singh01 <Maneesh_Singh01@ad.infosys.com>
2017-05-18 14:37:17 +0530

    Aise hi

--- MyFirstSpringApp/src/main/java/com/spring/controller/SpringAppController.java
+++ MyFirstSpringApp/src/main/java/com/spring/controller/SpringAppController.java
@@ -3,13 +3,20 @@ package com.spring.controller;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.service.SpringAppService;
 
 @Controller
-@RequestMapping("/controller")
+//@RequestMapping("/controller")
 public class SpringAppController {
-	
+
 	@Qualifier("springAppService")
 	private SpringAppService springAppService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String root() {
+		System.out.println("aaya yeha");
+		return "home";
+	}
 }
